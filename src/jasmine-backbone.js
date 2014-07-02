a5a7d81748696859deffcabafe9f3cd452253deb
mjeanroy <mickael.jeanroy@mos.gfit.fr>
2014-07-02 18:56:22 +0200

    init repository

---
+++ src/jasmine-backbone.js
@@ -0,0 +1,324 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Mickael Jeanroy <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+(function(_, Backbone, undefined) {
+
+  // Check version
+  var JASMINE_VERSION = (function() {
+    var version = jasmine.version_ || jasmine.version;
+    if (version.major) {
+      version = version.major;
+    } else {
+      version = parseInt(version.split('.')[0], 10);
+    }
+    return version;
+  })();
+
+  var IS_JASMINE_1 = JASMINE_VERSION === 1;
+  var IS_JASMINE_2 = JASMINE_VERSION === 2;
+
+  var MODEL_METHODS = ['fetch', 'save', 'destroy', 'trigger'];
+  var COLLECTION_METHODS = ['fetch', 'trigger'];
+  var VIEW_METHODS = ['initialize', 'render', 'listenToOnce', 'listenTo', 'remove'];
+
+  var pp = function(message) {
+    var placeholders = [].slice.call(arguments, 1);
+    for (var i = 0, size = placeholders.length; i < size; ++i) {
+      if (placeholders.hasOwnProperty(i)) {
+        message = message.replace('{{%' + i + '}}', jasmine.pp(placeholders[i]));
+      }
+    }
+    return message;
+  };
+
+  var andCallThrough = function(spy) {
+    if (IS_JASMINE_1) {
+      spy.andCallThrough();
+    } else {
+      spy.and.callThrough();
+    }
+    return spy;
+  };
+
+  var spyIf = function(obj, methodName) {
+    if (!jasmine.isSpy(obj[methodName])) {
+      var spy = spyOn(obj, methodName);
+      andCallThrough(spy);
+    }
+    return obj[methodName];
+  };
+
+  var spyEach = function(obj, methods) {
+    if (!_.isArray(methods)) {
+      methods = [methods];
+    }
+
+    _.each(methods, function(methodName) {
+      spyIf(obj, methodName);
+    });
+  };
+
+  var isInstanceOf = function(obj, Klass) {
+    return obj instanceof Klass;
+  };
+
+  var isBackboneModel = function(obj) {
+    return isInstanceOf(obj, Backbone.Model);
+  };
+
+  var isBackboneCollection = function(obj) {
+    return isInstanceOf(obj, Backbone.Collection);
+  };
+
+  var isBackboneView = function(obj) {
+    return isInstanceOf(obj, Backbone.View);
+  };
+
+  jasmine.Backbone = {
+    useMock: function() {
+      spyEach(Backbone.View.prototype, VIEW_METHODS);
+      spyEach(Backbone.Model.prototype, MODEL_METHODS);
+      spyEach(Backbone.Collection.prototype, COLLECTION_METHODS);
+    },
+
+    mockModel: function(model, methods) {
+      methods = MODEL_METHODS.concat(methods || []);
+      spyEach(model, methods);
+    },
+
+    mockCollection: function(collection, methods) {
+      methods = COLLECTION_METHODS.concat(methods || []);
+      spyEach(collection, methods);
+    },
+
+    mockView: function(view, methods) {
+      methods = VIEW_METHODS.concat(methods || []);
+      spyEach(view, methods);
+    }
+  };
+
+  var matchers = {
+    toBeABackboneModel: function() {
+      return {
+        pass: isBackboneModel(this.actual),
+        message: pp('Expect {{%0}} {{not}} to be a backbone model', this.actual)
+      };
+    },
+
+    toBeABackboneCollection: function() {
+      return {
+        pass: isBackboneCollection(this.actual),
+        message: pp('Expect {{%0}} {{not}} to be a backbone collection', this.actual)
+      };
+    },
+
+    toBeABackboneView: function() {
+      return {
+        pass: isBackboneView(this.actual),
+        message: pp('Expect {{%0}} {{not}} to be a backbone view', this.actual)
+      };
+    },
+
+    toHaveModelAttribute: function(name, value) {
+      var actual = this.actual;
+      var checkValue = arguments.length === 2;
+      return {
+        pass: isBackboneModel(actual) && actual.has(name) && (!checkValue || this.equals(actual.get(name), value)),
+        message: pp('Expect {{%0}} {{not}} to have attribute {{%1}}' + checkValue ? ' equal to {{%2}}' : '', actual, name, value)
+      };
+    },
+
+    toHaveModelURL: function(url) {
+      return {
+        pass: _.result(this.actual, 'url') === url,
+        message: pp('Expect model {{not}} to have url {{%0}}', url)
+      };
+    },
+
+    toHaveCollectionURL: function(url) {
+      return {
+        pass: _.result(this.actual, 'url') === url,
+        message: pp('Expect collection {{not}} to have url {{%0}}', url)
+      };
+    },
+
+    toHaveModelURLRoot: function(urlRoot) {
+      return {
+        pass: _.result(this.actual, 'urlRoot') === urlRoot,
+        message: pp('Expect model {{not}} to have root url {{%0}}', urlRoot)
+      };
+    },
+
+    toHaveViewClassName: function(className) {
+      return {
+        pass: _.result(this.actual, 'className') === className,
+        message: pp('Expect view {{not}} to have class name {{%0}}', className)
+      };
+    },
+
+    toHaveViewTagName: function(tagName) {
+      return {
+        pass: _.result(this.actual, 'tagName') === tagName,
+        message: pp('Expect view {{not}} to have tag name {{%0}}', tagName)
+      };
+    },
+
+    toHaveBeenFetched: function() {
+      // TODO
+    },
+
+    toHaveBeenSaved: function() {
+      // TODO
+    },
+
+    toHaveBeenDestroyed: function() {
+      // TODO
+    },
+
+    toListenTo: function() {
+      // TODO
+    },
+
+    toListenToOnce: function() {
+      // TODO
+    }
+  };
+
+  var jasmineMatchers = {};
+
+  var parseNegateMessage = function(isNot, message) {
+    var notKey = isNot ? '{{not}}' : '{{not}} ';
+    var notValue = isNot ? 'not' : '';
+    return (message || '').replace(notKey, notValue);
+  };
+
+  var toJasmineMatcher = {
+    1: function(fn) {
+      return function() {
+        var env = this.env;
+        var equals_ = this.env.equals_;
+
+        var ctx = {
+          actual: this.actual,
+          isNot: this.isNot,
+          callCount: function(spy) {
+            return spy.callCount;
+          },
+          argsFor: function(spy, call) {
+            return spy.argsForCall[call];
+          },
+          equals: function() {
+            return equals_.apply(env, arguments);
+          }
+        };
+
+        var result = fn.apply(ctx, arguments);
+
+        // Not yet implemented
+        if (!result) {
+          return false;
+        }
+
+        var isNot = this.isNot;
+
+        if (!result.pass) {
+          this.message = function() {
+            return parseNegateMessage(isNot, result.message);
+          };
+        }
+
+        return result.pass;
+      };
+    },
+
+    2: function(fn) {
+      return function(util, customEqualityTesters) {
+        var ctx = {
+          callCount: function(spy) {
+            return spy.calls.count();
+          },
+          argsFor: function(spy, call) {
+            return spy.calls.argsFor(call);
+          },
+          equals: function(a, b) {
+            return util.equals(a, b, customEqualityTesters);
+          }
+        };
+
+        return {
+          compare: function(actual) {
+            ctx.actual = actual;
+            ctx.isNot = false;
+
+            var args = [].slice.call(arguments, 1);
+            var result = fn.apply(ctx, args);
+
+            // Not yet implemented
+            if (!result) {
+              return false;
+            }
+
+            return {
+              pass: result.pass,
+              message: parseNegateMessage(false, result.message)
+            };
+          },
+
+          negativeCompare: function(actual) {
+            ctx.actual = actual;
+            ctx.isNot = true;
+
+            var args = [].slice.call(arguments, 1);
+            var result = fn.apply(ctx, args);
+
+             // Not yet implemented
+            if (!result) {
+              return true;
+            }
+
+            return {
+              pass: !result.pass,
+              message: parseNegateMessage(true, result.message)
+            };
+          }
+        };
+      };
+    }
+  };
+
+  for (var matcher in matchers) {
+    if (matchers.hasOwnProperty(matcher)) {
+      jasmineMatchers[matcher] = toJasmineMatcher[JASMINE_VERSION](matchers[matcher]);
+    }
+  }
+
+  beforeEach(function() {
+    if (IS_JASMINE_1) {
+      this.addMatchers(jasmineMatchers);
+    } else {
+      jasmine.addMatchers(jasmineMatchers);
+    }
+  });
+
+})(_, Backbone, void 0);
---
+++ test/jasmine-backbone-spec.js
@@ -0,0 +1,144 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Mickael Jeanroy <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+describe('Jasmine-Backbone', function() {
+
+  beforeEach(function() {
+    jasmine.addMatchers({
+      toBeASpy: function(util, customEqualityTesters) {
+        return {
+          compare: function(actual) {
+            return {
+              pass: jasmine.isSpy(actual)
+            };
+          }
+        }
+      }
+    });
+  });
+
+  describe('installation', function() {
+    it('should mock backbone views methods', function() {
+      jasmine.Backbone.useMock();
+
+      var ViewProto = Backbone.View.prototype;
+      expect(ViewProto.initialize).toBeASpy();
+      expect(ViewProto.render).toBeASpy();
+      expect(ViewProto.listenToOnce).toBeASpy();
+      expect(ViewProto.listenTo).toBeASpy();
+      expect(ViewProto.remove).toBeASpy();
+    });
+
+    it('should mock backbone models methods', function() {
+      jasmine.Backbone.useMock();
+
+      var ModelProto = Backbone.Model.prototype;
+      expect(ModelProto.fetch).toBeASpy();
+      expect(ModelProto.save).toBeASpy();
+      expect(ModelProto.destroy).toBeASpy();
+      expect(ModelProto.trigger).toBeASpy();
+    });
+
+    it('should mock backbone collections methods', function() {
+      jasmine.Backbone.useMock();
+
+      var CollectionProto = Backbone.Collection.prototype;
+      expect(CollectionProto.fetch).toBeASpy();
+      expect(CollectionProto.trigger).toBeASpy();
+    });
+  });
+
+  describe('once installed', function() {
+    describe('with views', function() {
+      beforeEach(function() {
+        this.View = Backbone.View.extend({
+        });
+
+        this.view = new this.View();
+      });
+    });
+
+    describe('with models', function() {
+      beforeEach(function() {
+        this.Model1 = Backbone.Model.extend({
+          defaults: {
+            foo : 'bar'
+          },
+
+          urlRoot: '/foo'
+        });
+
+        this.Model1 = Backbone.Model.extend({
+          defaults: {
+            foo : 'bar'
+          },
+
+          urlRoot: function() {
+            return '/foo';
+          }
+        });
+
+        this.model1 = new this.Model1({
+          bar: 'foo',
+          sub: {
+            id: 1
+          }
+        });
+
+        this.model2 = new this.Model1({
+        	id: 1,
+          bar: 'foo'
+        });
+      });
+
+      it('should check if a model has an attribute', function() {
+        expect(this.model1).toHaveModelAttribute('foo');
+        expect(this.model1).toHaveModelAttribute('bar');
+        expect(this.model1).not.toHaveModelAttribute('foobar');
+      });
+
+      it('should check if a model has an attribute with expected value', function() {
+        expect(this.model1).toHaveModelAttribute('foo', 'bar');
+        expect(this.model1).toHaveModelAttribute('bar', 'foo');
+        expect(this.model1).toHaveModelAttribute('sub', {
+          id: 1
+        });
+
+        expect(this.model1).not.toHaveModelAttribute('foo', 'foo');
+        expect(this.model1).not.toHaveModelAttribute('bar', 'bar');
+        expect(this.model1).not.toHaveModelAttribute('sub', {
+          id: 2
+        });
+      });
+
+      it('should check model root URL', function() {
+        expect(this.model1).toHaveModelURLRoot('/foo');
+        expect(this.model2).toHaveModelURLRoot('/foo');
+
+        expect(this.model1).not.toHaveModelURLRoot('/bar');
+        expect(this.model2).not.toHaveModelURLRoot('/bar');
+      });
+   });
+  });
+});
